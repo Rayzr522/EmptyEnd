8b9636c5c9fa73c54ed1c86575d977c8b02791c2
Rayzr522 <rayzr522@gmail.com>
2017-02-02 16:03:39 -0500

    Project 'EmptyEnd' generated
    Generated using CNLPluginArchetype and custom CLI project generator tool by Rayzr

---
+++ README.md
@@ -0,0 +1,4 @@
+# EmptyEnd
+Prevent endermen from spawning in the end.
+
+*Note: This README is a pre-generated stub*
---
+++ src/main/java/com/rayzr522/emptyend/EmptyEnd.java
@@ -0,0 +1,15 @@
+package com.rayzr522.emptyend;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Rayzr
+ */
+public class EmptyEnd extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
