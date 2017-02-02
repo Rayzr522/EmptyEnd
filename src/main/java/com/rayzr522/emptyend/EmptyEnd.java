50b33f55b1c3a2a885551f8715b5ea75a9c3a12b
Rayzr522 <rayzr522@gmail.com>
2017-02-02 16:10:11 -0500

    v1.0.0-FINAL :P

--- src/main/java/com/rayzr522/emptyend/EmptyEnd.java
+++ src/main/java/com/rayzr522/emptyend/EmptyEnd.java
@@ -1,14 +1,27 @@
 package com.rayzr522.emptyend;
 
+import org.bukkit.World.Environment;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * @author Rayzr
  */
-public class EmptyEnd extends JavaPlugin {
+public class EmptyEnd extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onSpawn(CreatureSpawnEvent e) {
+        if (e.getLocation().getWorld().getEnvironment() == Environment.THE_END && e.getSpawnReason() == SpawnReason.NATURAL || e.getSpawnReason() == SpawnReason.CHUNK_GEN) {
+            e.setCancelled(true);
+        }
 
     }
 
